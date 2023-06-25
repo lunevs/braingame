603b1de6870932660cec0d372ddb7a2c6215e948
Sergey Lunev <lunev.s@gmail.com>
2023-06-25 23:53:17 +0300

    first commit

---
+++ src/main/java/com/ichtus/braingame/BraingameApplication.java
@@ -0,0 +1,13 @@
+package com.ichtus.braingame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraingameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraingameApplication.class, args);
+	}
+
+}
